2ed92a12d9c5d32566253c8d47e37459569feabd
Makoto Emura <makoto@emurasoft.com>
2023-06-29 13:17:26 -0700

    Revert "Parallel jobs"
    This reverts commit aaca5a3126028f399b265b027401f35ad2dbbc37.

--- buildAll.sh
+++ buildAll.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-pids=()
-PROJECT='en' sphinx-build . _build/en & pids+=($!)
-PROJECT='ja' sphinx-build . _build/ja & pids+=($!)
-PROJECT='ko' sphinx-build . _build/ko & pids+=($!)
-PROJECT='zh-cn' sphinx-build . _build/zh-cn & pids+=($!)
-PROJECT='zh-tw' sphinx-build . _build/zh-tw & pids+=($!)
-
-error=false
-for pid in ${pids[*]}; do
-    if ! wait $pid; then
-        error=true
-    fi
-done
-
-if error; then
-    echo "Error occurred"
-    exit 1
-fi
+PROJECT='en' sphinx-build . _build/en
+PROJECT='ja' sphinx-build . _build/ja
+PROJECT='ko' sphinx-build . _build/ko
+PROJECT='zh-cn' sphinx-build . _build/zh-cn
+PROJECT='zh-tw' sphinx-build . _build/zh-tw
