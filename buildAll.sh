8386ded91900a82708d49acbb4ee149d0981ce31
Makoto Emura <makoto@emurasoft.com>
2023-07-08 22:16:54 -0700

    buildAll.sh

--- buildAll.sh
+++ buildAll.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-PROJECT='en' sphinx-build . _build/en
-PROJECT='ja' sphinx-build . _build/ja
-PROJECT='ko' sphinx-build . _build/ko
-PROJECT='zh-cn' sphinx-build . _build/zh-cn
-PROJECT='zh-tw' sphinx-build . _build/zh-tw
-
-parallel 'PROJECT={} sphinx-build . _build/{}' ::: en ja ko zh-cn zh-tw'
+parallel 'PROJECT={} sphinx-build . _build/{} ::: en ja ko zh-cn zh-tw'
