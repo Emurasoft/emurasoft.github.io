e4c66a00ad9f902ed7975d2591d98b461f52ffc2
Makoto Emura <makoto@emurasoft.com>
2023-07-28 17:10:01 -0700

    Add redirect pages

--- create-redirects/create_redirects.go
+++ create-redirects/create_redirects.go
@@ -10,20 +10,9 @@ import (
 )
 
 func main() {
-	language := `en`
-	paths := getPaths(`..\` + language)
-
-	for _, path := range paths {
-		newPath := strings.ReplaceAll(path, `\`, "_")
-		newPath = fmt.Sprintf(`..\_build\%s\%s.html`, language, newPath)
-
-		if err := os.MkdirAll(filepath.Dir(newPath), 0666); err != nil {
-			log.Panicln(err)
-		}
-
-		if err := os.WriteFile(newPath, []byte{}, 0666); err != nil {
-			log.Panic(err)
-		}
+	for _, language := range []string{"en", "ja", "ko", "zh-cn", "zh-tw"} {
+		paths := getPaths(`..\` + language)
+		writeRedirects(language, paths)
 	}
 }
 
@@ -52,4 +41,23 @@ func getPaths(rootPath string) []string {
 	}
 
 	return pathsToRedirect
+}
+
+func writeRedirects(language string, paths []string) {
+	for _, path := range paths {
+		newPath := strings.ReplaceAll(path, `\`, "_")
+		newPath = fmt.Sprintf(`..\_build\%s\%s.html`, language, newPath)
+
+		if err := os.MkdirAll(filepath.Dir(newPath), 0666); err != nil {
+			log.Panicln(err)
+		}
+
+		const redirectFormat = `<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0;url=https://emurasoft.github.io/emeditor-help/%s/%s.html"></head></html>`
+
+		redirectText := fmt.Sprintf(redirectFormat, language, filepath.ToSlash(path))
+
+		if err := os.WriteFile(newPath, []byte(redirectText), 0666); err != nil {
+			log.Panic(err)
+		}
+	}
 }
