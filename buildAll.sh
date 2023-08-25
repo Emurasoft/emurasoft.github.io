ba1140b8e4d2e914237693b5d4a1bed248c3f083
Makoto Emura <makoto@emurasoft.com>
2023-08-24 18:20:45 -0700

    html_search_scorer

--- buildAll.sh
+++ buildAll.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-parallel --verbose 'PROJECT={} sphinx-build -vvv --jobs auto . _build/{}' ::: en ja ko zh-cn zh-tw
+parallel --verbose 'PROJECT={} sphinx-build -vv --jobs auto . _build/{}' ::: en ja ko zh-cn zh-tw
