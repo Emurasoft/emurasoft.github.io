528aa21afe42baee9250046d2743446791f72aa2
Makoto Emura <makoto@emurasoft.com>
2023-09-09 18:25:40 -0700

    html_baseurl

--- ko/conf.py
+++ ko/conf.py
@@ -1,1 +1,2 @@
 sitemap_url_scheme = 'ko/{version}{link}'
+html_baseurl = 'https://emurasoft.github.io/emeditor-help/ko/'
--- zh-cn/conf.py
+++ zh-cn/conf.py
@@ -1,1 +1,2 @@
 sitemap_url_scheme = 'zh-cn/{version}{link}'
+html_baseurl = 'https://emurasoft.github.io/emeditor-help/zh-cn/'
