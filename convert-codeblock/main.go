1bc5b4d4aaf17442732361faece8ffa30842cdf2
Makoto Emura <makoto@emurasoft.com>
2023-11-04 18:34:57 -0700

    convert-codeblock

---
+++ convert-codeblock/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	err := filepath.WalkDir(
+		`C:\Users\MakotoEmura\Documents\emeditor-help\en\plugin\message`,
+		walkFunc,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func walkFunc(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if d.Type().IsDir() {
+		return nil
+	}
+
+	text := ""
+	{
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		text = string(bytes)
+	}
+
+	split := splitText(text)
+
+	// Replace double newline
+	split.Codeblock = strings.ReplaceAll(split.Codeblock, "\r\n\r\n", "\r\n")
+
+	resultText := split.Head + "```\r\n" + split.Codeblock + "```\r\n" + split.End
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(resultText)
+	return err
+}
+
+type SplitText struct {
+	Head      string
+	Codeblock string
+	End       string
+}
+
+func splitText(text string) *SplitText {
+	title, body := splitTitle(text)
+
+	// Find second instance of title - Beginning of codeblock
+	titleIndex := strings.Index(body, title)
+	if titleIndex < 0 {
+		log.Panicf("title not found: %s", title)
+	}
+
+	afterTitle := body[titleIndex:]
+
+	parametersIndex := strings.Index(afterTitle, "\r\n## Parameters")
+	if parametersIndex < 0 {
+		log.Panicf("parameters not found: %s", afterTitle)
+	}
+
+	return &SplitText{
+		Head:      "# " + title + body[:titleIndex],
+		Codeblock: afterTitle[:parametersIndex],
+		End:       afterTitle[parametersIndex:],
+	}
+}
+
+func splitTitle(text string) (title string, body string) {
+	text = strings.TrimLeft(text, "# ")
+	i := strings.Index(text, "\r\n")
+	if i < 0 {
+		log.Panicf("separator not found: %s", text)
+	}
+	return text[:i], text[i:]
+}
