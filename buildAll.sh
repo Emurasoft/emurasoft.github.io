7c4c8090003a0741e6850e5133eb45872c674395
Makoto Emura <makoto@emurasoft.com>
2024-02-18 14:08:35 -0800

    fix script

--- buildAll.sh
+++ buildAll.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-parallel --verbose 'PROJECT={} sphinx-build -v --jobs auto -W --keep-going -n . _build/{}' ::: en ja ko zh-cn zh-tw | tee output.txt
 
+# Build all projects in parallel. Ignore error.
+exit_code=0
+(parallel --verbose 'PROJECT={} sphinx-build -v --jobs auto -W --keep-going -n . _build/{}' ::: en ja ko zh-cn zh-tw | tee output.txt) || exit_code=$?
+
+# Output error messages
 cat output.txt | grep 'WARNING:\|ERROR:'
+
+# Exit with exit code from build
+exit "$exit_code"
