dd89b5f0a1bf676c7f3fccdcfe44ed4b1cfbd31b
Makoto Emura <makoto@emurasoft.com>
2025-04-29 14:12:21 -0700

    wrap_emojis_in_tex

--- buildPDF.py
+++ buildPDF.py
@@ -130,6 +130,8 @@ def wrap_emojis_in_tex(tex_file):
 
     emoji_pattern = re.compile(
         r'(['
+        '\U00002600-\U000026FF'
+        '\U00002700-\U000027BF'
         '\U0001F300-\U0001F5FF'
         '\U0001F600-\U0001F64F'
         '\U0001F680-\U0001F6FF'
@@ -139,7 +141,6 @@ def wrap_emojis_in_tex(tex_file):
         '\U0001F900-\U0001F9FF'
         '\U0001FA00-\U0001FA6F'
         '\U0001FA70-\U0001FAFF'
-        '\U00002702-\U000027B0'
         '])'
     )
 
